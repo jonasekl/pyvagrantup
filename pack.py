b132d75d0b4f80f9c00720928b6dfc83b98bb487
Jonas Eklöf <jonas.eklof@svt.se>
2015-05-05 09:50:24 +0200

    fixed typo in pack.py

--- pack.py
+++ pack.py
@@ -5,11 +5,16 @@ class Packager(object):
         self.params = yaml.load(open('./metadata/%s.yml' % box).read())
 
     def package(self):
+        self._move_old_package()
         ret_value = self._run_package_command()
         print 'ret_value:', ret_value
         if not ret_value:
             self._bump_box_version()
         self._save_yaml()
+
+    def _move_old_package(self):
+        if os.path.exists('./boxes/%s.box' % self.params.get('name')):
+            os.rename('./boxes/%s.box' % self.params.get('name'), './boxes/%s_%d.box' % (self.params.get('name'), self.params.get('version')))
 
     def _run_package_command(self):
         return subprocess.call(['vagrant', 'package', '--base', self.params.get('base'), '--output', './boxes/%s.box' % self.params.get('name')])
@@ -18,7 +23,7 @@ class Packager(object):
         self.params['version'] += 1
 
     def _save_yaml(self):
-        with open('./metadata/%s.yml', 'w') as yml:
+        with open('./metadata/%s.yml' % self.params.get('name'), 'w') as yml:
             yml.write(yaml.dump(self.params, default_flow_style=False))
 
 if __name__=='__main__':
